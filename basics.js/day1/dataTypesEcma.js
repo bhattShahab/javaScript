0035d59a2bba31757b4bb7817375c52ee2cc55c4
bhattShahab <123869301+bhattShahab@users.noreply.github.com>
2023-11-24 19:22:11 +0000

    Let Var const and dataTypes and ecma standard

---
+++ basics.js/day1/dataTypesEcma.js
@@ -0,0 +1,20 @@
+"use strict" //treat all the js code as newer version
+
+// alert(3+3) //we are using nodejs not browser
+
+// number => upto 2^53
+// bigint
+// boolean => true or false
+// string=""
+// null=> standalone value
+// undefined
+// symbol=> unique
+
+//object
+
+// console.log(typeof null) ===>object ,  ********. kinda error?  /******
+// console.log(undefined)
+
+//follow mdn documentation
+
+
---
+++ basics.js/day1/letVarConst.js
@@ -0,0 +1,9 @@
+var accountPassword="751289102345" //THERE WAS A BLOCK SCOPE AND FUNCTIONAL SCOPE, PROBLEM SO BETTER TO USE LET ONLY FOR VARIABLES
+accountCity="Delhi" // THIS IS NEVER ADVISED TO DO
+
+const accountId="2234234234"
+let accountEmail="akash@gmail.com"
+
+let akash //undefined if not initialized
+
+console.table([accountId,accountEmail,accountPassword,accountCity,akash])
