c8ba2afc174476c9aa1f4bdb298b6642528ef667
bhattShahab <123869301+bhattShahab@users.noreply.github.com>
2023-11-27 09:37:47 +0000

    Numbers And Maths

---
+++ basics.js/day2/numbersAndMaths.js
@@ -0,0 +1,54 @@
+//Numbers
+
+// const score=400
+// console.log(score)
+// console.log(typeof score); //Number
+
+
+//to exculusively mention the datatype
+
+// const balance=new Number(100)
+// console.log(balance) // [Number:100]
+// console.log(typeof balance) //object
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(2)); 100.00
+
+const otherNumber=2345.8955
+
+//This tells how many values we need after precision
+// console.log(otherNumber.toPrecision(3));
+const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN')); In Indian format
+
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.MAX_VALUE);
+
+// Maths********************************************************
+
+// console.log(Math) Object
+
+// console.log(Math.abs(-4.5));
+// console.log(Math.round(4.55));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.999999999999999)); but one more 9 at 
+//end give you 5 ,dont know why ?
+
+// console.log(Math.min(1,2,4,5,7,0));
+// console.log(Math.max(1,2,4,5,7,0));
+
+//Math.random() //lies between [0 and 1]
+
+// console.log(Math.random());
+
+// console.log(Math.floor(Math.random()*10)+1) //1-10
+
+//If we want to get random number in a particular range
+
+// const min=10
+// const max=20
+// console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
