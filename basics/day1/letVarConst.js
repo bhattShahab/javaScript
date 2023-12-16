ce1c6d80366e74284fc1b6cd7ec75184bb2b2bf8
bhattShahab <123869301+bhattShahab@users.noreply.github.com>
2023-12-16 10:38:20 +0000

    why not to use var

--- basics/day1/letVarConst.js
+++ basics/day1/letVarConst.js
@@ -7,3 +7,25 @@ let accountEmail="akash@gmail.com"
 let akash //undefined if not initialized
 
 console.table([accountId,accountEmail,accountPassword,accountCity,akash])
+
+// Why not var 
+
+// var was there from very beginning
+
+// ecma 2017  -->let works for all faults present with var
+
+//let cannot be redeclared
+
+var a=10
+var a=13
+
+let b=10
+// let b=13//error
+
+//let ---->block scope , var ----->functional scope.  //memory consumption more with var
+
+
+// --->could not make the changes with var because a lot of websites had already been designed using var keyword that could had trouble with those sites
+
+
+
