33bfd3a74e8652f85c2e187fbe77340a8ab4dc5a
bhattShahab <123869301+bhattShahab@users.noreply.github.com>
2023-12-17 13:19:00 +0000

    closure,funciton expression,basic about hoisting

---
+++ scopes/02_scopes.js
@@ -0,0 +1,19 @@
+//closure????
+//function expression
+
+//child block can acces parent block variables
+//parent block can't access child block variables
+
+// console.log(add1(2,3)); hoisting is not done with function expression
+// const add1=function (num1,num2) {
+//     return num1+num2
+// }
+
+// console.log(add2(2,3)); //hoisting is  done with function expression
+
+function add2(num1,num2){
+    return num1+num2
+}
+
+// console.log(add2(1,2));
+
