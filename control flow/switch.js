cd6d29a056ee938f3665d4797560bdbfc9221c53
bhattShahab <123869301+bhattShahab@users.noreply.github.com>
2023-12-18 01:44:32 +0000

    use of switch block

---
+++ control flow/switch.js
@@ -0,0 +1,34 @@
+//Syntax
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+//Note if we do not use the break,then all the statements
+//after the condition matched would be executed,except default
