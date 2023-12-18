7b9ada13337c83f917e80a202c4e44bf9cbffa3b
bhattShahab <123869301+bhattShahab@users.noreply.github.com>
2023-12-18 19:11:54 +0000

    understanding of map in array

---
+++ iterations/seven.js
@@ -0,0 +1,16 @@
+const myNumbers=[1,2,3,4,5,6,7,8,9,10]
+
+//return the value
+
+// const newNums=myNumbers.map((num)=>num+10)
+
+// console.log(newNums);
+
+//chaining
+
+// const newNums2=myNumbers
+//                     .map((num)=>num*10)
+//                     .map((num)=>num+1)
+//                     .filter((num)=>num>40)
+
+// console.log(newNums2);
