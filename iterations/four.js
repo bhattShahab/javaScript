cb9c465af509746cb1d0b1446a29423ec2bf71ce
bhattShahab <123869301+bhattShahab@users.noreply.github.com>
2023-12-18 18:49:20 +0000

    understanding of for in loop

---
+++ iterations/four.js
@@ -0,0 +1,21 @@
+const myObj={
+    js:'javascript',
+    cpp:'c++',
+    rb:'rubby'
+}
+
+// for (const key in myObj){
+//     console.log(key);
+// }
+
+// for(const key in myObj){
+//     console.log(myObj[key]);
+// }
+
+// for in loop gives key in array also 
+
+//for of loop gives value
+
+
+
+
